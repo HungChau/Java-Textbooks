a61f0cf1a03a5a4d6ef32f4902a78150c1125774
HUNG CHAU <hungchaukim@gmail.com>
2017-05-29 01:38:42 -0400

    chapter 5

---
+++ Java-Foundation/chap05-writing-classes/Textbook-This-Reference.java
@@ -0,0 +1,9 @@
+  if (this.position == piece2.position)
+      result =false;
+      
+       public Account (String name, long acctNumber, double balance)
+  {
+      this.name = name;
+      this.acctNumber = acctNumber;
+      this.balance = balance;
+}
